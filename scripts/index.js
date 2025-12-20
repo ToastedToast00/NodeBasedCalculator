d9bf1f1d4849c66927cb10f3a4db2ecbe15b01b1
ToastedToast0090 <toastedtoast0090@gmail.com>
2025-12-20 01:14:05 -0800

    Added import statement on Graph.js to pull from Node.js, added import statement on index.js to initialize the graph

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,9 @@
-// JavaScript source code
+import { Graph } from "./models/Graph";
+
+// create the graph instance
+const graph = new Graph
+
+//expose for debugging in console
+window.graph = graph;
+
+console.log("Graph initialized:", graph)
--- scripts/models/Graph.js
+++ scripts/models/Graph.js
@@ -1,3 +1,5 @@
+import { Node } from "./Node";
+
 export class Graph 
 {
     constructor()
